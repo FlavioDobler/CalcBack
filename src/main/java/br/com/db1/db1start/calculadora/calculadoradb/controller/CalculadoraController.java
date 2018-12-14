fb2c8fff14a6f4a030255e323faf59b907a498cc
FlavioDobler <45239464+FlavioDobler@users.noreply.github.com>
2018-12-14 17:39:58 -0200

    Update CalculadoraController.java

--- src/main/java/br/com/db1/db1start/calculadora/calculadoradb/controller/CalculadoraController.java
+++ src/main/java/br/com/db1/db1start/calculadora/calculadoradb/controller/CalculadoraController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import br.com.db1.db1start.calculadora.calculadoradb.model.Calculadora;
 import br.com.db1.db1start.calculadora.calculadoradb.model.Operacao;
 import br.com.db1.db1start.calculadora.calculadoradb.service.CalculadoraService;
 
+
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/calculadora")
 public class CalculadoraController {
